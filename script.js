d9048f5c6e0fed1b5358fb84aa45436a08582288
IrfanCatovic <catovicc84@gmail.com>
2023-05-26 02:28:22 +0200

    Email check

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 "use strict";
 
-function testing() {
-  let link = document.querySelector(".drugi-link").innerText;
+// function testing() {
+//   let link = document.querySelector(".drugi-link").innerText;
+//   let gl_div = document.querySelector(".glavni-div");
 
-  console.log(link);
-}
+//   link.setAttribute('class', 'plava-boja') mozemo da menjamo atribute nekog elementa
+//   console.log(link.getAttribute("data-test")); dobijamo vrednost ovog atributa
+//   link.removeAttribute("data-test"); ukoliko zelimo da sklonimo atribut
+
+//   gl_div.innerHTML = '<span>Klikni ovaj link: <a href="#">klik </span>';
+
+//   console.log(link);
+// }
+
+//Validacija login
+// function Validacija() {
+//   let input = document.querySelector("#email");
+//   let value = input.value;
+
+//   if (value.includes("@") && value.includes(".")) {
+//     let pozicijaAt = value.indexOf("@");
+//     let pozicijaTacka = value.indexOf("."); //pokazuje poziciju . u reci koju pisemo
+
+//     console.log(pozicijaAt, pozicijaTacka);
+//     let izmedjuAtTacka = value.substring(pozicijaAt + 1, pozicijaTacka); // pokazuje nam sta je izmedju @ i tacke
+
+//     console.log(typeof izmedjuAtTacka.lenght);
+//     console.log(izmedjuAtTacka.lenght);
+//     if (izmedjuAtTacka.lenght > 0) {
+//       console.log("Dobro je");
+//     } else {
+//       console.log("nije dobro");
+//     }
+//   } else {
+//     console.log("Mail nije validan");
+//   }
+// }

341eebb72594a12ec38a5b5f4c1a9957973e1284
IrfanCatovic <catovicc84@gmail.com>
2023-05-28 02:50:31 +0200

    Zavrsen cart

--- script.js
+++ script.js
@@ -34,6 +34,9 @@
 
 //I can use this function on every button bcs of action onClick(this) - it sends informations of div where button is clicked
 //
+
+let allTotal = 0;
+
 function addToCart(element) {
   let mainEl = element.closest(".single-item");
   let price = mainEl.querySelector(".price").innerText;
@@ -47,10 +50,17 @@ function addToCart(element) {
 
     let total = price * parseInt(kol);
 
-    cartItems.innerHTML += `prizvod: ${name} -
-    cijena: ${price} -
-    Količina ${kol}
-    Total: ${total}`;
+    allTotal += total;
+
+    cartItems.innerHTML += `
+
+    <div class="cart-single-item"> 
+      <h3>${name}</h3>
+      <p>${price} x ${kol} = $<span>${total}</span></p>
+      <button onclick="removeFromCart(this)" class="remove-item">Ukloni</button>
+    </div>`;
+
+    document.querySelector(".total").innerText = `Total = ${allTotal}`;
 
     element.innerText = "Dodato";
     element.setAttribute("disabled", "true");
@@ -58,5 +68,27 @@ function addToCart(element) {
     alert("Odaberi kolicinu");
   }
 
+  function removeFromCart(element) {
+    let mainEl = element.closest(".cart-single-item");
+    let price = mainEl.querySelector("p span").innerText;
+    let name = mainEl.querySelector("h3").innerText;
+    let povrce = document.querySelectorAll(".single-item");
+
+    price = parseInt(price);
+    allTotal -= price;
+
+    document.querySelector(".total").innerText = `Total: $${allTotal}`;
+
+    mainEl.remove();
+
+    povrce.forEach(function (vege) {
+      let itemName = vege.querySelector(".si-content h3").innerText;
+      if (itemName === name) {
+        vege.querySelector(".actions input").value = 0;
+        vege.querySelector(".actions button").removeAttribute(".disabled");
+        vege.querySelector(".actions button").innerText = "Dodaj";
+      }
+    });
+  }
   // let input = element.previousElementSibling;
 }

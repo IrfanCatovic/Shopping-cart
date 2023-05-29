6403a1f38e26cf7d7dadbf56f5eac5fe526c2511
IrfanCatovic <catovicc84@gmail.com>
2023-05-29 12:57:45 +0200

    Buttone problem wont go disable

---
+++ Movie/scriptMovie.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let allTotal = 0;
+
+function gledao(element) {
+  let mainEl = element.closest(".card");
+  let price = mainEl.querySelector(".card-price").innerText;
+
+  price = price.substring(8); //We could make price <h3> and just substring 1 but we left it this way
+  price = parseFloat(price);
+
+  allTotal += price;
+  let zaokruzena = allTotal.toFixed(2);
+  document.querySelector(".total-price").innerText = `$ ${zaokruzena}`;
+
+  element.innerText = "Watched";
+  element.style.backgroundColor = "green";
+  element.style.color = "white";
+  element.setAttribute("disabled", "true");
+
+  mainEl.innerHTML += `
+
+  <div class=".cnt-rmv"> 
+
+    <button onclick="removeMovie(this)" class=".btn-rmv">Remove</button>
+  </div>`;
+}
+
+function removeMovie(element) {
+  console.log("Remove");
+}

b82dfd4513dfe36c60e1914f0f7b91cd461b1f84
IrfanCatovic <catovicc84@gmail.com>
2023-05-31 15:44:55 +0200

    event listener

---
+++ bubnjevi-main/bubnjevi-main/script.js
@@ -0,0 +1,54 @@
+// const klikni = (dugme) => {
+//     console.log(dugme.innerText);
+// }
+
+// let btns = document.querySelectorAll('button');
+
+// btns.forEach(function(btn){
+
+
+//     //event listener koji ceka za svaki klik, za svaki button
+//     btn.addEventListener("click", function(event){
+//         let clicked_btn = event.target;
+
+//         console.log(clicked_btn.innerText);
+
+//     });
+// });
+
+// let link = document.querySelector('#link');
+
+// link.addEventListener('click', (event) => {
+// event.preventDefault();
+
+// console.log(event.target);
+// })
+
+// let form = document.querySelector('form');
+
+// form.addEventListener('submit', (event) => {
+    
+//     event.preventDefault();
+
+//     console.log('test');
+
+
+// });
+
+// let opcija = document.querySelector('select');
+
+// opcija.addEventListener("change", (event) => {
+//     console.log(event.target.value);
+// });
+
+// window.addEventListener("resize", () => {
+//     console.log(window.innerWidth);
+
+//     if(window.innerWidth > 1000){
+//         console('Prozor veci od 1000');
+//     }
+//     else {
+//         console.log('Prozor manji od 1000');
+//     }
+// });
+

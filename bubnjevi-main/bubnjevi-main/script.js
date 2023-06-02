5d7583837c53a75c679433008e86bda279b73241
IrfanCatovic <catovicc84@gmail.com>
2023-06-02 19:16:01 +0200

    Animation in css

--- bubnjevi-main/bubnjevi-main/script.js
+++ bubnjevi-main/bubnjevi-main/script.js
@@ -50,6 +50,17 @@
 //     }
 // });
 
+let crashRide = document.querySelector("#crash-ride");
+let hiHatTop = document.querySelector("#hihat-top");
+
+const animateCrashOrRide = () => {
+  crashRide.getElementsByClassName.transform = "rotate(0deg) scale(1.5)";
+};
+
+const animateHiHatClosed = () => {
+  hiHatTop.style.top = "171px";
+};
+
 window.addEventListener("keydown", (event) => {
   let code = event.keyCode;
   let keyElement = document.querySelector(`div[data-key="${code}"]`);
@@ -59,4 +70,29 @@ window.addEventListener("keydown", (event) => {
   let audio = document.querySelector(`audio[data-key="${code}"]`);
   audio.ariaCurrentTime = 0; //kad god pritisnemo vracamo ga na 0, ponovo jer inace nece da ucita 2x brzo
   audio.play();
+
+  switch (code) {
+    case 69:
+    case 82:
+      animateCrashOrRide();
+      break;
+    case 75:
+      animateHiHatClosed();
+      break;
+  }
 });
+
+const removeCrashRideTransition = (e) => {
+  if (e.propertyName !== "transform") return;
+
+  e.target.style.transform = "rotate(-7.2deg) scale(1.5)";
+};
+
+const removeHiHatTopTransition = (e) => {
+  if (e.propertyName !== "top") return;
+
+  e.target.classList.remove("playing");
+};
+
+crashRide.addEventListener("transitionend", removeCrashRideTransition);
+hiHatTop.addEventListener("transitionend", removeHiHatTopTransition);
